ace8cec9dbaa62cdf20adfa37e1ee64ce50cac64
Diana Krutiakova <diana06119@gmail.com>
2020-05-03 18:29:18 +0300

    Enter pyramid 1

--- mario.cpp
+++ mario.cpp
@@ -34,3 +34,18 @@ int main()
 		while (cin.get() != '\n');
 	}
 	cout << "Option = " << option << endl;
+	cout << "Your pyramid\n";
+	if (option == 1)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				if (i + j >= height - 1)
+					cout << "#";
+				else
+					cout << " ";
+			}
+			cout << endl;
+		}
+	}
