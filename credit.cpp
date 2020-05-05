dd64dd299c1daa731198bf0c233a60b16b08d348
Mariia Boiarchuk <mboiar2004@gmail.com>
2020-05-05 22:44:42 +0300

    Checking Input

--- credit.cpp
+++ credit.cpp
@@ -57,3 +57,32 @@ void ShowCardNumber(const char NumberCard[], const int SIZE_CARD)
 			cout << " ";
 	}
 }
+
+void CheckInput(char NumberCard[], const int SIZE_CARD)
+{
+	bool exit = true;
+
+	do
+	{
+		cout << "Enter number card 16 characters\n";
+		cin.getline(NumberCard, SIZE_CARD + 1);
+
+		NumberCard[SIZE_CARD] = '\0';
+
+		for (size_t i = 0; i < SIZE_CARD; i++)
+		{
+			if (NumberCard[i] >= 0 && !(NumberCard[i] > ':') && !(NumberCard[i] < '/'))
+			{
+				if (i == SIZE_CARD - 1)
+					exit = false;
+			}
+			else
+			{
+				cout << "Incorrect input. Please try again\n";
+				cin.clear();
+				while (cin.get() != '\n');
+				break;
+			}
+		}
+	} while (exit);
+}
