992018bb1a192409ac3d45311225858497d833d9
Mariia Boiarchuk <mboiar2004@gmail.com>
2020-05-05 22:25:32 +0300

    Checking the correctness of the card

--- credit.cpp
+++ credit.cpp
@@ -24,3 +24,25 @@ int main()
 
 	return 0;
 }
+
+bool CorrectCard(const char* NumberCard)
+{
+	int Sum = 0;
+	int Size = strlen(NumberCard);
+	int Parity = (Size - 1) % 2;
+	char ZeroSymb[2] = "\0";
+
+	for (int i = Size; i > 0; i--)
+	{
+		ZeroSymb[0] = NumberCard[i - 1];
+		int Digit = atoi(ZeroSymb);
+
+		if (Parity == i % 2)
+			Digit = Digit * 2;
+
+		Sum += Digit / 10;
+		Sum += Digit % 10;
+	}
+
+	return 0 == Sum % 10;
+}
